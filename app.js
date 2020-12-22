136251f18754cc857852b2bdbc8d021302ff430e
abhishek agrawal <agrawalabhisehk960@gmail.com>
2020-12-22 21:49:38 +0530

    just updated

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
-const http=require("http");
+const express=require('express');
+const app=express();
+const port=3000;
+app.listen(port,function(error){
+    if(error){
+        console.log("error")
+
+    }
+    else{
+        console.log("Port is running 3000")
+    }
+})
+app.get("/",function(req,res){
+    res.send('ravi is a good man');
+})
