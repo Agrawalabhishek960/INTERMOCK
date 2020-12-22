dc63270c2cef1376ee967438c76a273dc0f33210
Ravi Kumar <rganga757@gmail.com>
2020-12-22 20:09:35 +0530

    major commit

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
-const http=require("http");
+const express=require('express');
+const app=express();
+const port=3000;
+
+app.get("/",function(req,res){
+    res.send('<h1>Ayushi I love u yaar.<br>Come back to my life')
+
+})
+app.listen(port,function(error){
+    if(error){
+        console.log("error")
+
+    }
+    else{
+        console.log("Port is up and running")
+    }
+})
